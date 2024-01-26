323f608b401cae83119beaf58abb5fd0f89d5de3
itamm15 <osinski.mateusz15@gmail.com>
2024-01-26 18:08:26 +0100

    reuse menus in the MainMenu

--- src/main/java/org/mateuszosinski/menu/Menu.java
+++ src/main/java/org/mateuszosinski/menu/Menu.java
@@ -6,12 +6,6 @@ import org.mateuszosinski.seeds.Seeds;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static org.mateuszosinski.classrooms.classroom.methods.*;
-import static org.mateuszosinski.grades.grade.methods.*;
-import static org.mateuszosinski.people.legalGuardian.methods.createLegalGuardian;
-import static org.mateuszosinski.people.student.methods.*;
-import static org.mateuszosinski.people.teacher.methods.*;
-
 public class Menu {
     public static void showMenu(ArrayList<DatabaseObject> database) {
         boolean isRunning = true;
@@ -19,21 +13,13 @@ public class Menu {
         while(isRunning) {
             System.out.println(
                     "Pick the action to proceed!\n" +
-                            "1. Create a Teacher\n" +
-                            "2. Update a Teacher\n" +
-                            "3. Delete a Teacher\n" +
-                            "4. Create a Student\n" +
-                            "5. Update a Student\n" +
-                            "6. Delete a Student\n" +
-                            "7. Create a legal guardian for student\n" +
-                            "8. Create a grade\n" +
-                            "9. Update a grade\n" +
-                            "10. Delete a grade\n" +
-                            "11. Show all Teachers\n" +
-                            "12. Show all Students\n" +
-                            "13. Show all Grades\n" +
-                            "14. Exit.\n" +
-                            "15. Seed it!"
+                            "1. Teacher actions\n" +
+                            "2. Student actions\n" +
+                            "3. Grades actions\n" +
+                            "4. Classrooms actions\n" +
+                            "5. Legal Guardian actions\n" +
+                            "6. Seed it!\n" +
+                            "7. Exit."
             );
 
             Scanner scanner = new Scanner(System.in);
@@ -43,58 +29,26 @@ public class Menu {
 
                 switch (option) {
                     case 1:
-                        createTeacher(database);
+                        TeacherMenu.run(database);
                         break;
                     case 2:
-
+                        StudentMenu.run(database);
                         break;
                     case 3:
-                        deleteTeacher(database);
+                        GradeMenu.run(database);
                         break;
                     case 4:
-                        createStudent(database);
+                        // CLASSROOM
                         break;
                     case 5:
-
+                        LegalGuardianMenu.run(database);
                         break;
                     case 6:
-                        deleteStudent(database);
+                        Seeds.run(database);
                         break;
                     case 7:
-                        createLegalGuardian(database);
-                        break;
-                    case 8:
-                        createClassroom(database);
-                        break;
-                    case 9:
-                        assignStudentToClassroom(database);
-                        break;
-                    case 10:
-                        showGivenClassroom(database);
-                        break;
-                    case 11:
-                        createGrade(database);
-                        break;
-                    case 12:
-                        deleteGrade(database);
-                        break;
-                    case 13:
-                        showTeachers(database);
-                        break;
-                    case 14:
-                        showStudents(database);
-                        break;
-                    case 15:
-                        showGrades(database);
-                        break;
-                    case 16:
-                        showClassrooms(database);
-                        break;
-                    case 17:
                         isRunning = false;
-                        break;
-                    case 18:
-                        Seeds.run(database);
+                        System.out.println("Bye!");
                         break;
                     default:
                         System.out.println("There is no such an option! Try again.");
