1eaf3a80c8ee96f4c847c292568795e40b9cdc9f
itamm15 <osinski.mateusz15@gmail.com>
2024-01-26 18:04:46 +0100

    move legalGuardian-related stuffs to legalGuardianMenu

--- src/main/java/org/mateuszosinski/menu/LegalGuardianMenu.java
+++ src/main/java/org/mateuszosinski/menu/LegalGuardianMenu.java
@@ -5,30 +5,22 @@ import org.mateuszosinski.databaseobject.DatabaseObject;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static org.mateuszosinski.grades.grade.methods.*;
+import static org.mateuszosinski.people.legalGuardian.methods.createLegalGuardian;
 
-public class GradeMenu {
+public class LegalGuardianMenu {
     public static void run(ArrayList<DatabaseObject> database) {
         try {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Select an option for Student\n" +
-                    "1. Create a grade\n" +
-                    "2. Delete a grade\n" +
-                    "3. Show all grades\n" +
-                    "4. Go back."
+                    "1. Create a legal guardian for student\n" +
+                    "2. Go back."
             );
 
             int chosenOption = scanner.nextInt();
 
             switch (chosenOption) {
                 case 1:
-                    createGrade(database);
-                    break;
-                case 2:
-                    deleteGrade(database);
-                    break;
-                case 3:
-                    showGrades(database);
+                    createLegalGuardian(database);
                     break;
                 default:
                     System.out.println("Going back!");
@@ -36,7 +28,7 @@ public class GradeMenu {
             }
 
         } catch (Exception exception) {
-            System.out.println("Something went wrong for the Grade menu!" + exception);
+            System.out.println("Something went wrong for the LegalGuardian menu!" + exception);
         }
     }
 }
